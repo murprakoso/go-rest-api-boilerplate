1bf8370eac6bae59a6cde013bc55369ba89a7cd4
murprakoso <prakoso.clan04@gmail.com>
2024-01-26 00:40:24 +0700

    feat(product): adding seeder

--- src/app.go
+++ src/app.go
@@ -5,11 +5,12 @@ import (
 	"log"
 )
 
+// App initializes and runs the application.
 func App() {
+	// Initialize the router
 	router := routers.Router()
 
-	//router.Run(":8080")
-	//gin.Logger("%v", router.Run("8080"))
+	// Run the server on port 8080
 	err := router.Run(":8080")
 	if err != nil {
 		log.Fatal("Error starting server:", err)
--- src/database/product_seeder.go
+++ src/database/product_seeder.go
@@ -1,7 +1,20 @@
 package database
 
-import "fmt"
+import (
+	"fmt"
+	"go-rest-api-boilerplate/src/commons/core"
+	entity "go-rest-api-boilerplate/src/modules/product"
+)
 
 func ProductSeeder() {
 	fmt.Println("Running Product Seeder")
+	product := entity.Product{}
+	product.Name = "Laptop 21 In"
+	product.Description = "The Description"
+	product.Price = 50000
+
+	err := core.DB.Create(&product).Error
+	if err != nil {
+		fmt.Println("Error creating product record!")
+	}
 }
